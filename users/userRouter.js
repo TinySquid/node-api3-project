820ff26376985cf7524a8a1662015a8852ab0180
Michael Nunes <miketnunes@gmail.com>
2019-12-03 15:57:57 -0800

    Add user validation middlewares

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const db = require('./userDb');
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -34,14 +36,46 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const id = req.params.id;
+
+  db.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user;
+      } else {
+        res.status(400).json({ message: "Invalid user id." });
+      }
+    })
+    .catch(error => {
+      next(new Error("Could not validate user id (database error)"));
+    });
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (req.body) {
+    if (req.body.name) {
+      next();
+    } else {
+      res.status(400).json({ message: "Missing required name field" });
+    }
+  } else {
+    res.status(400).json({ message: "Missing user data" });
+  }
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (req.body) {
+    if (req.body.text) {
+      next();
+    } else {
+      res.status(400).json({ message: "Missing required text field" });
+    }
+  } else {
+    res.status(400).json({ message: "Missing post data" });
+  }
+
 }
 
 module.exports = router;
