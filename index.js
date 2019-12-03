90a698e207929fccd9fc67051af8efa73b583d1d
Michael Nunes <miketnunes@gmail.com>
2019-12-03 14:04:43 -0800

    Project and server setup

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code away!
+// ok =)
+//For .env config
+require('dotenv').config();
+
+const express = require('express');
+
+const cors = require('cors');
+
+const port = process.env.PORT || 4000;
+
+const server = express();
+
+server.use(express.json());
+
+server.use(cors());
+
+
+
+
+
+
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
+
+
